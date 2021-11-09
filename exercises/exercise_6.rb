122a08a52e09a7b8b835bc0fc02a195df9904891
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-09 09:17:37 -0500

    Creating the employees db.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,43 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@s1.employees.create(
+  first_name: 'Jeb',
+  last_name: 'Bush',
+  hourly_rate: 600
+)
+
+@s2.employees.create(
+  first_name: 'Jimmy',
+  last_name: 'Carter',
+  hourly_rate: 1
+)
+
+@s2.employees.create(
+  first_name: 'Chad',
+  last_name: 'Kruger',
+  hourly_rate: 80
+)
+
+@s2.employees.create(
+  first_name: 'Eric',
+  last_name: 'Clapton',
+  hourly_rate: 23
+)
+
+@s1.employees.create(
+  first_name: 'Bob',
+  last_name: 'Marley',
+  hourly_rate: 40
+)
+
+@s1.employees.create(
+  first_name: 'Michael',
+  last_name: 'Jackson',
+  hourly_rate: 2
+)
+
+@s2.employees.create(
+  first_name: 'Jimmy',
+  last_name: 'Buffett',
+  hourly_rate: 10)
