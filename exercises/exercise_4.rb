831e4c39fb95287ededc46c3c22557cb4f152302
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-09 09:11:29 -0500

    Adding 3 new stores to db && fetch the stores which carry men's apparel && print all stores with it's name and it's annual revenue && fetch the stores which carry women's apparel and generating less than M in annual revenue.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,36 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+@s5 = Store.create(
+  name: 'Surrey',
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+@s4 = Store.create(
+  name: 'Whistler',
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+@s6 = Store.create(
+  name: 'Yaletown',
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each { |s| puts "#{s.name} - annual revenue: #{s.annual_revenue}" }
+
+@womens_high_volume_stores = Store.where(
+womens_apparel: true,
+annual_revenue: 0..1000000
+)
+
+@womens_high_volume_stores.each do |s|
+  puts "High-volume women's apparel stores:"
+  puts "#{s.name} - annual revenue: #{s.annual_revenue}"
+end
