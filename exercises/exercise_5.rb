e6860c9deba3769a9bfe0f6a8740d87fa5782c87
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-09 09:15:13 -0500

    Output the total revenue for the entire company (all stores) && output the average annual revenue for all stores && the number of stores that are generating M or more in annual sales.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum('annual_revenue')
+number_of_stores = Store.count
+high_volume_stores = Store.where("annual_revenue > ?", 1_000_000).size
+
+puts "Total revenue: $#{total_revenue}"
+puts "Average revenue per store: $#{total_revenue / number_of_stores}"
+puts "Stores with annual earnings in excess of $1,000,000: #{high_volume_stores}"
